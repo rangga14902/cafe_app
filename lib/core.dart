8fd5051c92f12a790665115a2fa9242e7f1b0e55
rangga14902 <ranggaghifarib@gmail.com>
2023-11-13 11:58:57 +0700

    first commit

---
+++ lib/core.dart
@@ -0,0 +1,10 @@
+/*
+We believe, the class name must be unique. 
+If there is a conflicting class name in this file,
+it means you have to rename it to something more unique.
+*/
+export 'package:coba/module/dashboard/controller/dashboard_controller.dart';
+export 'package:coba/module/dashboard/view/dashboard_view.dart';
+export 'package:coba/module/mainmenu/controller/mainmenu_controller.dart';
+export 'package:coba/module/mainmenu/view/mainmenu_view.dart';
+export 'package:coba/state_util.dart';
